b788ae50169b6bc4931981709663e4799591e115
Jackie Leany <113729205+FaeJackalope@users.noreply.github.com>
2025-01-29 17:09:58 -0500

    Create testing.js

---
+++ testing.js
@@ -0,0 +1,8 @@
+(function () {
+  "use strict";
+  var script = document.createElement("script");
+  script.type = "module";
+  script.setAttribute("crossorigin", "anonymous");
+  script.src = "https://127.0.0.1:5500/";
+  document.head.appendChild(script);
+})();
